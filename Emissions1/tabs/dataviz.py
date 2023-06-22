e766fb6d5b7e78a2f1ca11eb9a8a998a35775263
Márquez <108117107+jmarquez211@users.noreply.github.com>
2023-06-22 14:33:14 +0200

    Update dataviz.py

--- Emissions1/tabs/dataviz.py
+++ Emissions1/tabs/dataviz.py
@@ -25,7 +25,7 @@ def run():
     st.subheader('CO2 emissions dataset')
     
     
-    df = pd.read_csv('data2019_r1.csv')
+    df = pd.read_csv('data2019_r1')
     df = df.sample(frac=0.04)
     st.write(df.head())
     
